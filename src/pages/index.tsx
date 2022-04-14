6b045aaff43fd55bdffe1c04715086caaddc7b32
Giulia Fiorenzano <giulia.fiorenzano@alibin.com.br>
2022-04-14 15:12:20 -0300

    feat: add primeiro componente chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex as="form">
+        
+      </Flex>
+    </Flex>
+    
   )
 }
