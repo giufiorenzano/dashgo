05a5dc6ad4560cc090145f8d9e9b307a27eeaec8
giufiorenzano <giufiorenzano@gmail.com>
2022-04-16 18:13:13 -0300

    feat: primeira pg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <Flex 
-      w="100vw" 
-      h="100vh" 
-      align="center" 
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
       justify="center"
     >
-      <Flex as="form">
-        
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input type="email"  name="email" label="E-mail" />
+          <Input type="password"  name="password" label="Senha" />
+          
+
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg">Entrar</Button>
       </Flex>
     </Flex>
-    
+
   )
 }
