66bfa335452d992ae3037928cf003d082cd9f1fe
Giulia Fiorenzano <giulia.fiorenzano@alibin.com.br>
2022-05-23 12:48:46 -0300

    feat: trocando de pagina no pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -44,7 +44,7 @@ export function Pagination({
 
             {currentPage > (1 + siblingsCount) && (
                 <>
-                    <PaginationItem number={1} />
+                    <PaginationItem onPageChange={onPageChange} number={1} />
                     {currentPage > (2 + siblingsCount) && 
                     <Text
                         color="gray.300"
@@ -56,13 +56,13 @@ export function Pagination({
             )}
            
            {previousPages.length > 0 && previousPages.map(page => {
-               return <PaginationItem key={page} number={page} />
+               return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
            })}
 
-            <PaginationItem number={currentPage} isCurrent />
+            <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
             
             {nextPages.length > 0 && nextPages.map(page => {
-               return <PaginationItem key={page} number={page} />
+               return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
            })}
            </Stack>
 
@@ -73,7 +73,7 @@ export function Pagination({
                         width={8}
                         textAlign="center"
                     >...</Text>}
-                <PaginationItem number={lastPage} />
+                <PaginationItem onPageChange={onPageChange} number={lastPage} />
                </>
                 
             )}
